ce91a23f363f020243a98f34f8322cc501a4c249
yasuomaidana <yasuomaidana@gmail.com>
2023-03-03 16:31:16 +0900

    Creating pure C project

---
+++ pure_c/Readme.md
@@ -0,0 +1,5 @@
+# Notes
+
+To be able to run the project while debugging in vscode, please check that your launch.json has `"externalConsole": true,`
+
+To see all the configurations please check `.vscode` directory.
---
+++ pure_c/pure_c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+#define PI 3.14159
+
+void calculate_circle_area(double radius);
+
+int main()
+{
+    double radius = 0.0;
+    printf("Previous size of radious :%lu \n",sizeof(radius));
+    printf("Enter radius: ");
+    scanf("%lf", &radius);
+    printf("Size of radious after input: %lu \n",sizeof(radius));
+    calculate_circle_area(radius);
+}
+
+void calculate_circle_area(double radius) {
+    double area = PI * radius * radius;
+    printf("radius of %lf meters\n \tarea is %lf sq. meters \n", radius, area);
+}
