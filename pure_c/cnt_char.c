13e7de53c549e0a83296a301fe3f46284061cb90
yasuomaidana <yasuomaidana@gmail.com>
2023-03-04 18:45:40 +0900

    Using getchar

---
+++ pure_c/cnt_char.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int blanks = 0, digits = 0, letters = 0, others = 0;
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+            ++blanks;
+        else if (c >= '0' && c <= '9')
+            ++digits;
+        else if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+            ++letters;
+        else
+            ++others;
+    }
+    printf(" blanks: %d \ndigits: %d\n letters: %d\nothers: %d", blanks, digits, letters, others);
+}
