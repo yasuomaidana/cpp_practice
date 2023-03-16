b3eb8ff18b7eecd534b58012f7712c157d3e136d
yasuomaidana <yasuomaidana@gmail.com>
2023-03-16 16:30:38 +0900

    Adding methods to list

--- pure_c/excercises/abstract data types/list.c
+++ pure_c/excercises/abstract data types/list.c
@@ -23,6 +23,27 @@ void print_list(list *h, char *title)
     }
 }
 
+list* create_list(int d){
+    list* head = malloc(sizeof(list));
+    head -> data = d;
+    head -> next=NULL;
+    return head;
+}
+
+list* add_to_front(int d, list *h){
+    list* head = create_list(d);
+    head->next = h;
+    return head;
+}
+
+list* array_to_list(int d[], int size){
+    list* head = create_list(d[0]);
+    for(int i=1; i<size; i++){
+        head = add_to_front(d[i],head);
+    }
+    return head;
+}
+
 int main(void)
 {
     list list_of_int;
@@ -36,5 +57,11 @@ int main(void)
     head->next = NULL;
 
     print_list(head, "single element list");
+    
+
+    int data[] = {2,3,4,5,9,10};
+    head = array_to_list(data,6);
+    print_list(head, "Multiple elements list");
+
     free(head);
 }
